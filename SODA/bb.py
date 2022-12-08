0b987a2238d43a9815a148f035566fac3cbbd9c7
Shanice <stb2145@columbia.edu>
2022-12-08 00:16:57 +0000

    added slicing function to module

--- SODA/bb.py
+++ SODA/bb.py
@@ -12,7 +12,7 @@ def box_budget(da_i, da_j, left_i, right_i, lower_j, upper_j, bs=None):
     right = da_i.isel(xu_ocean=right_i, yt_ocean=slice(lower_j, upper_j+1)).rename('right')
     left = da_i.isel(xu_ocean=left_i-1, yt_ocean=slice(lower_j, upper_j+1)).rename('left')
     
-    if bs == 'upper':
+    if bs=='upper':
         return upper
     elif bs=='lower':
         return lower
@@ -21,7 +21,27 @@ def box_budget(da_i, da_j, left_i, right_i, lower_j, upper_j, bs=None):
     elif bs=='left':
         return left
     else:
-        print('total: ', -upper.sum('xt_ocean') + lower.sum('xt_ocean') - right.sum('yt_ocean') + left.sum('yt_ocean'))
+        None #print('total: ', -upper.sum('xt_ocean') + lower.sum('xt_ocean') - right.sum('yt_ocean') + left.sum('yt_ocean'))
     
     
     return -upper.sum('xt_ocean') + lower.sum('xt_ocean') - right.sum('yt_ocean') + left.sum('yt_ocean')
+
+
+#Slicing instead of slice and sum
+
+def slice_box(da, left_i=None, right_i=None, lower_j=None, upper_j=None, cell=None):
+    
+    """Function to simply slice data to desired box boundary.
+        Specify if data array's grid is only grid center, or
+        some combination of grid center and face."""
+    
+    if cell=='c':
+        box = da.isel(yt_ocean=slice(lower_j, upper_j), xt_ocean=slice(left_i, right_i))
+    elif cell=='xg':
+        box = da.isel(yt_ocean=slice(lower_j, upper_j), xu_ocean=slice(left_i, right_i))
+    elif cell=='yg':
+        box = da.isel(yu_ocean=slice(lower_j, upper_j), xt_ocean=slice(left_i, right_i))
+    else:
+        None
+    
+    return box
